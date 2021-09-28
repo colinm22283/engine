548835465ec115487141ccd18da0e6913bede147
Colin M <colinm22283@isd273.org>
2021-09-28 14:10:43 -0400

    experimental scene build

--- header/script.h
+++ header/script.h
@@ -8,14 +8,12 @@ class Script
 public:
     Script();
 
-    //void start();
-    //void update();
-    //void keyDown(SDL_Keysym keysym);
-    //virtual void mouseDown(int button);
-    //virtual void mouseClick(int button);
+    void start();
+    void update();
+    void keyDown(SDL_Keysym keysym);
+    void mouseDown(int button);
+    void mouseClick(int button);
     void exit();
 
     static void callExit();
-//protected:
-    static std::vector<Script> scripts;
 };
--- source/script.cpp
+++ source/script.cpp
@@ -1,28 +1,30 @@
 #include "SDL/SDL.h"
+#include <iostream>
 
 #include "script.h"
 #include "engine.h"
 #include "console.h"
 
+std::vector<Script> scripts;
+
 Script::Script()
 {
-    Script::scripts.push_back(*this);
+    scripts.push_back(*this);
 }
 
-void Script::exit()
+void Script::exit() { }
+void Script::update()
 {
-    Console::print("Exit");
-    Engine::quit = true;
+    Console::print("Update");
 }
-// void Script::update()
-// {
-//     Console::print("Update");
-// }
 
 void Script::callExit()
 {
-    for (int i = 0; i < (int)Script::scripts.size(); i++)
+    for (int i = 0; i < (int)scripts.size(); i++)
     {
-        Script::scripts[i].exit();
+        std::cout << "test\n";
+        scripts[i].exit();
     }
+
+    Engine::quit = true;
 }
